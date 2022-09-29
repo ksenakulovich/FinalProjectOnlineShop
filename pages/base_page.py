9d548c55a746893df037c20ade935f9a59ef7a2b
Kseniya Akulovich <ksenakulovich@gmail.com>
2022-09-29 11:28:25 +0300

    Login page initialization was added explicitly to the file with tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -18,3 +18,4 @@ class BasePage:
         return True
 
 
+
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 
+
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
@@ -8,6 +9,7 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not present"
+
 
 
 
